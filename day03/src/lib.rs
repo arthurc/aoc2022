6aaf194c1a3f96a940531ccdc3a8bbf39cb3444d
Arthur Hartwig Carlsson <arthur@kiron.net>
2022-12-05 22:08:54 +0000

    day03 part2

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 
 pub fn calculate_combined_priority_for_shared_items(s: &str) -> u32 {
     s.lines()
-        .flat_map(|l| Rucksack::parse(l.trim(), 2))
+        .flat_map(|l| Rucksack::parse(l, 2))
         .map(|r| {
             r.shared_item_occurrences()
                 .keys()
@@ -12,13 +12,31 @@ pub fn calculate_combined_priority_for_shared_items(s: &str) -> u32 {
         .sum()
 }
 
+pub fn calculate_badge_priority_for_groups<const N: usize>(s: &str) -> u32 {
+    s.lines()
+        .flat_map(|l| Rucksack::parse(l, 2))
+        .scan(Vec::new(), |state, x| {
+            state.push(x);
+
+            if state.len() == N {
+                Some(Some(Group::new(state.drain(..).collect())))
+            } else {
+                Some(None)
+            }
+        })
+        .flat_map(|o| o.into_iter())
+        .flat_map(|g| g.badge().map(|b| b.priority()))
+        .sum()
+}
+
 #[derive(Debug, PartialEq)]
 struct Rucksack {
     compartments: Vec<Compartment>,
 }
 impl Rucksack {
     fn parse(s: &str, compartment_count: usize) -> Option<Self> {
-        if s.len() % compartment_count != 0 {
+        let s = s.trim();
+        if s.is_empty() || s.len() % compartment_count != 0 {
             return None;
         }
 
@@ -45,6 +63,15 @@ impl Rucksack {
                 map
             })
     }
+
+    fn item_occurrences(&self) -> HashMap<Item, usize> {
+        self.compartments.iter().fold(HashMap::new(), |mut map, c| {
+            for (item, _) in c.item_occurrences() {
+                *map.entry(item).or_insert(0) += 1;
+            }
+            map
+        })
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -56,6 +83,13 @@ impl Compartment {
 
     fn contains(&self, item: &Item) -> bool {
         self.items().any(|other| other == *item)
+    }
+
+    fn item_occurrences(&self) -> HashMap<Item, usize> {
+        self.items().fold(HashMap::new(), |mut map, item| {
+            *map.entry(item).or_insert(0) += 1;
+            map
+        })
     }
 }
 
@@ -72,6 +106,32 @@ impl Item {
         } else {
             self.0 as u32 - 'A' as u32 + 27
         }
+    }
+}
+
+#[derive(Debug)]
+struct Group {
+    rucksacks: Vec<Rucksack>,
+}
+impl Group {
+    fn new(rucksacks: Vec<Rucksack>) -> Self {
+        Self { rucksacks }
+    }
+
+    fn badge(&self) -> Option<Item> {
+        self.item_occurrences()
+            .into_iter()
+            .find(|(_, v)| *v == self.rucksacks.len())
+            .map(|(a, _)| a)
+    }
+
+    fn item_occurrences(&self) -> HashMap<Item, usize> {
+        self.rucksacks.iter().fold(HashMap::new(), |mut map, r| {
+            for (item, _) in r.item_occurrences() {
+                *map.entry(item).or_insert(0) += 1;
+            }
+            map
+        })
     }
 }
 
@@ -159,6 +219,50 @@ mod tests {
                 ttgJtRGJQctTZtZT
                 CrZsJsPPZsGzwwsLwLmpwMDw
                 "
+            )
+        );
+    }
+
+    #[test]
+    fn test_calculate_group_badge() {
+        let group1 = Group::new(
+            "
+            vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+            "
+            .lines()
+            .flat_map(|l| Rucksack::parse(l, 2))
+            .collect(),
+        );
+        assert_eq!(Some(Item('r')), group1.badge());
+
+        let group2 = Group::new(
+            "
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+            "
+            .lines()
+            .flat_map(|l| Rucksack::parse(l, 2))
+            .collect(),
+        );
+        assert_eq!(Some(Item('Z')), group2.badge());
+    }
+
+    #[test]
+    fn test_calculate_badge_priority_for_groups_example() {
+        assert_eq!(
+            70,
+            calculate_badge_priority_for_groups::<3>(
+                "vJrwpWtwJgWrhcsFMMfFFhFp
+            jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+            PmmdzqPrVvPwwTWBwg
+
+            wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+            ttgJtRGJQctTZtZT
+            CrZsJsPPZsGzwwsLwLmpwMDw
+        "
             )
         );
     }
--- day03/src/main.rs
+++ day03/src/main.rs
@@ -3,4 +3,8 @@ fn main() {
         "part1: {}",
         day03::calculate_combined_priority_for_shared_items(include_str!("../input.txt"))
     );
+    println!(
+        "part2: {}",
+        day03::calculate_badge_priority_for_groups::<3>(include_str!("../input.txt"))
+    );
 }
